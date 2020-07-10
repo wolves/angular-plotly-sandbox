8a959f8d7df6af3445ff34b872f7192faa845dee
Wolves <cstingl@protonmail.com>
2020-07-10 13:05:19 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,166 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'plotly-spike';
+
+  trace1 = {
+    x: [
+      1995,
+      1996,
+      1997,
+      1998,
+      1999,
+      2000,
+      2001,
+      2002,
+      2003,
+      2004,
+      2005,
+      2006,
+      2007,
+      2008,
+      2009,
+      2010,
+      2011,
+      2012,
+    ],
+    y: [
+      219,
+      146,
+      112,
+      127,
+      124,
+      180,
+      236,
+      207,
+      236,
+      263,
+      350,
+      430,
+      474,
+      526,
+      488,
+      537,
+      500,
+      439,
+    ],
+    name: 'Location 1',
+    marker: { color: 'rgb(55, 83, 109)' },
+    type: 'bar',
+  };
+
+  trace2 = {
+    x: [
+      1995,
+      1996,
+      1997,
+      1998,
+      1999,
+      2000,
+      2001,
+      2002,
+      2003,
+      2004,
+      2005,
+      2006,
+      2007,
+      2008,
+      2009,
+      2010,
+      2011,
+      2012,
+    ],
+    y: [
+      16,
+      13,
+      10,
+      11,
+      28,
+      37,
+      43,
+      55,
+      56,
+      88,
+      105,
+      156,
+      270,
+      299,
+      340,
+      403,
+      549,
+      499,
+    ],
+    name: 'Location 2',
+    marker: { color: 'rgb(26, 118, 255)' },
+    type: 'bar',
+  };
+
+  public graph = {
+    data: [this.trace1, this.trace2],
+    layout: {
+      title: 'Grouped Bar Chart',
+      xaxis: {
+        tickfont: {
+          size: 14,
+          color: 'rgb(107, 107, 107)',
+        },
+      },
+      yaxis: {
+        title: 'Value (millions)',
+        titlefont: {
+          size: 16,
+          color: 'rgb(107, 107, 107)',
+        },
+        tickfont: {
+          size: 14,
+          color: 'rgb(107, 107, 107)',
+        },
+      },
+      legend: {
+        x: 0,
+        y: 5.0,
+        bgcolor: 'rgba(255, 255, 255, 0)',
+        bordercolor: 'rgba(255, 255, 255, 0)',
+      },
+      barmode: 'group',
+      bargap: 0.15,
+      bargroupgap: 0.1,
+    },
+  };
+
+  // ========================
+
+  line1 = {
+    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    y: [10, 15, null, 17, 14, 12, 10, null, 15, 12, null, 18, 16],
+    mode: 'lines+markers',
+    connectgaps: true,
+  };
+
+  line2 = {
+    x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    y: [16, null, 13, 10, 8, null, 11, 2, 18, 4, null, 18, 16],
+    mode: 'lines',
+    connectgaps: true,
+  };
+
+  public lineGraph = {
+    config: {
+      displayModeBar: false,
+    },
+    data: [this.line1, this.line2],
+    layout: {
+      title: 'Line Chart w/ Connected Gaps (displayModeBar: false)',
+      showlegend: false,
+      yaxis: {
+        visible: true,
+        fixedrange: true,
+        tick0: 0,
+        dtick: 2,
+      },
+    },
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+import { PlotlyViaCDNModule } from 'angular-plotly.js';
+
+PlotlyViaCDNModule.plotlyVersion = '1.49.4'; // can be `latest` or any version number (i.e.: '1.40.0')
+PlotlyViaCDNModule.plotlyBundle = 'basic'; // optional: can be null (for full) or 'basic', 'cartesian', 'geo', 'gl3d', 'gl2d', 'mapbox' or 'finance'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, PlotlyViaCDNModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
